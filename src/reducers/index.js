619b454ea94f29cda2961636795d7be0ff5d8ee0
Amir Sohi <amirasohi@gmail.com>
2019-10-01 18:42:01 +0100

    Implemented redux

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import filmReducer from "./filmReducer/reducer";
+
+const rootReducer = combineReducers({
+  filmReducer
+});
+
+export default rootReducer;
